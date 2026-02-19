6913d20695449c818994222ade43298d7f7c683c
SSS <nelsinho.afreitas@gmail.com>
2026-02-19 12:30:49 -0300

    Criação da tela e jogo GalaxyDefender

--- Controllers/HomeController.cs
+++ Controllers/HomeController.cs
@@ -16,6 +16,11 @@ namespace SSSite.Controllers
             return View();
         }
 
+        public IActionResult GalacticDefender()
+        {
+            return View("~/Views/Jogos/GalaxyDefender.cshtml");
+        }
+
         [ResponseCache(Duration = 0, Location = ResponseCacheLocation.None, NoStore = true)]
         public IActionResult Error()
         {
