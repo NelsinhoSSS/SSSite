42328d18a98d2f3ef2f2f4bc71aba02d9f3802d9
SSS <nelsinho.afreitas@gmail.com>
2026-02-23 10:53:45 -0300

    Criação da parte de charts do site

---
+++ Controllers/ChartsController.cs
@@ -0,0 +1,17 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SeuProjeto.Controllers
+{
+    public class ChartsController : Controller
+    {
+        public IActionResult Pizza()
+        {
+            return View();
+        }
+
+        public IActionResult Column()
+        {
+            return View();
+        }
+    }
+}
