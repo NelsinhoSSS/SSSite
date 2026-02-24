61fada932cc89b1333bd1454f63b2a084e12813e
SSS <nelsinho.afreitas@gmail.com>
2026-02-24 11:46:06 -0300

    Criação da parte Random do site.

---
+++ Controllers/RandomController.cs
@@ -0,0 +1,37 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System;
+
+namespace SeuProjeto.Controllers
+{
+    public class RandomController : Controller
+    {
+        // Action para escolher um animal
+        public IActionResult Animais(string animalNome)
+        {
+            ViewBag.AnimalEscolhido = animalNome;
+            return View();
+        }
+
+
+
+        // Action para sortear um número
+        public IActionResult Numeros(bool sortear = false)
+        {
+            if (sortear)
+            {
+                Random rand = new Random();
+                // Sorteia entre 1 e 100.000.000
+                int resultado = rand.Next(1, 100000001);
+
+                // .ToString() sem parâmetros remove os pontos de milhar
+                ViewBag.NumeroSorteado = resultado.ToString();
+            }
+            else
+            {
+                ViewBag.NumeroSorteado = "--";
+            }
+
+            return View();
+        }
+    }
+}
