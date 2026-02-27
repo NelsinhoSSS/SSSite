90bc217d4bd2cd497f14525161949a913dc79f0e
SSS <nelsinho.afreitas@gmail.com>
2026-02-27 11:05:36 -0300

    Criação da parte do curriculo.

---
+++ Controllers/CurriculoController.cs
@@ -0,0 +1,18 @@
+﻿using Microsoft.AspNetCore.Mvc;
+
+namespace SSSite.Controllers
+{
+    public class CurriculoController : Controller
+    {
+        public IActionResult Index()
+        {
+            // Definindo o nome exato do seu arquivo
+            string nomeArquivo = "Curriculo_Nelson_Abreu_Freitas.pdf";
+
+            // Passando o caminho para a View (assumindo que está em wwwroot/curriculos/)
+            ViewBag.CaminhoPdf = $"/curriculos/{nomeArquivo}";
+
+            return View();
+        }
+    }
+}
